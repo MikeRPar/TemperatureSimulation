da3c2e8282954f8853c1cfc9cbff415047bb79f2
Michael Parlacoski <parlacoskim@msoe.edu>
2024-06-05 15:34:59 -0500

    stuff

--- TemperatureSimulation/GraphicsEngine.cpp
+++ TemperatureSimulation/GraphicsEngine.cpp
@@ -5,6 +5,10 @@
 
 #include <string.h>
 #include <cstring>
+#include <stdint.h>
+#include <stdexcept>
+#include <vector>
+#include <iostream>
 
 void GraphicsEngine::run()
 {
@@ -37,7 +41,54 @@ GraphicsEngine::GraphicsEngine(const char* name)
 //These functions set up engine and its resources
 void GraphicsEngine::initVulkan()
 {
+	createInstance();
+}
 
+//Vulkan Related Functions
+void GraphicsEngine::createInstance()
+{
+	//Application Info
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = windowName;
+	appInfo.applicationVersion = VK_MAKE_VERSION(PROGRAM_MAJOR, PROGRAM_MINOR, PROGRAM_PATCH);
+	appInfo.pEngineName = "No Engine"; //Using a custom implementation
+	appInfo.engineVersion = VK_MAKE_VERSION(ENGINE_MAJOR, ENGINE_MINOR, ENGINE_PATCH);
+	appInfo.apiVersion = VK_API_VERSION_1_0; //Select Vulkan Version
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+	createInfo.enabledLayerCount = 0;
+
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+	{
+		throw std::runtime_error("instance creation failed!");
+	}
+}
+
+//Detail function helpers
+
+//Prints optional extensions for Vulkan Instance
+void GraphicsEngine::printOptionalExtensions() {
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	for (const auto& extension : extensions)
+	{
+		std::cout << '\t' << extension.extensionName << '\n';
+	}
 }
 
 //Function sets up the window for the program with GLFW
@@ -60,6 +111,8 @@ void GraphicsEngine::mainLoop()
 
 void GraphicsEngine::cleanup()
 {
+	vkDestroyInstance(instance, nullptr);
+
 	glfwDestroyWindow(window);
 
 	glfwTerminate();
--- TemperatureSimulation/GraphicsEngine.h
+++ TemperatureSimulation/GraphicsEngine.h
@@ -8,6 +8,16 @@
 #define DEFAULT_WIDTH 800
 #define DEFAULT_HEIGHT 600
 
+//Program Version Details
+#define PROGRAM_MAJOR 1
+#define PROGRAM_MINOR 0
+#define PROGRAM_PATCH 0
+
+//Engine Version Details
+#define ENGINE_MAJOR 1
+#define ENGINE_MINOR 0
+#define ENGINE_PATCH 0
+
 class GraphicsEngine {
 	public:
 		GraphicsEngine();
@@ -16,11 +26,17 @@ class GraphicsEngine {
 	private:
 		//PRIVATE CLASS MEMBERS
 		GLFWwindow* window;
-		char* windowName; //Should not change during runtime
+		char* windowName; //Should not change during runtime, can't make const though
+
+		VkInstance instance; 
 
 		//SET UP FUNCTIONS
 		void initVulkan();
 		void initWindow();
+		void createInstance();
+
+		//Detail Checking
+		void printOptionalExtensions();
 
 		void mainLoop();
 		void cleanup();
